fc018b72b2bf1a38d852543f3f74614785b82f56
root <piotrwasiewiczeu@gmail.com>
2018-05-30 18:25:12 +0200

    softfloat trial

--- Makefile
+++ Makefile
@@ -27,7 +27,9 @@ kernel.syms: kernel.elf
 	$(Q)$(PRETTY) SYMS $(MODULE_NAME) $@
 	$(Q)$(OBJDUMP) -t kernel.elf > $@
 
-kernel.elf: LDFLAGS += -L "/usr/lib/gcc/armv7a-hardfloat-linux-gnueabi/7.3.0" -lgcc
-kernel.elf: LDFLAGS += -L "/usr/armv7a-hardfloat-linux-gnueabi/usr/lib" -lc
+#kernel.elf: LDFLAGS += -L "/usr/lib/gcc/armv7a-hardfloat-linux-gnueabi/7.3.0" -lgcc
+#kernel.elf: LDFLAGS += -L "/usr/armv7a-hardfloat-linux-gnueabi/usr/lib" -lc
+kernel.elf: LDFLAGS += -L "/usr/lib/gcc/arm-softfloat-linux-gnueabi/8.1.0" -lgcc
+kernel.elf: LDFLAGS += -L "/usr/arm-softfloat-linux-gnueabi/usr/lib" -lc
 kernel.elf: $(OBJECTS)
 	$(Q)$(LD) $(OBJECTS) -Map kernel.map -o $@ -T $(LINKER_SCRIPT) $(LDFLAGS)
--- dbuild.config.mk
+++ dbuild.config.mk
@@ -3,13 +3,14 @@ RASPPI	?= 2
 ifeq ($(strip $(RASPPI)),1)
 ARCH	?= -march=armv6j -mtune=arm1176jzf-s -mfloat-abi=hard 
 else
-ARCH	?= -march=armv7-a -mtune=cortex-a7 -mfloat-abi=hard
+#ARCH	?= -march=armv7-a -mtune=cortex-a7 -mfloat-abi=hard
+ARCH	?= -march=armv7-a -mtune=cortex-a7 -mfloat-abi=softfp
 endif
 
 AFLAGS ?= $(ARCH) -DRASPPI=$(RASPPI)
-#CFLAGS += $(ARCH) -g -std=gnu99 -Wno-psabi -fsigned-char -DRASPPI=$(RASPPI) -nostdlib -Wno-implicit -mfloat-abi=softfp 
-CFLAGS += $(ARCH) -g -std=gnu99 -Wno-psabi -fsigned-char -DRASPPI=$(RASPPI) -nostdlib -Wno-implicit -mfloat-abi=hard
-## CFLAGS += -finstrument-functions
+CFLAGS += $(ARCH) -g -std=gnu99 -Wno-psabi -fsigned-char -DRASPPI=$(RASPPI) -nostdlib -Wno-implicit -mfloat-abi=softfp 
+#CFLAGS += $(ARCH) -g -std=gnu99 -Wno-psabi -fsigned-char -DRASPPI=$(RASPPI) -nostdlib -Wno-implicit -mfloat-abi=hard 
+CFLAGS += -finstrument-functions
 CFLAGS += -mno-unaligned-access
 CFLAGS += -I $(BASE)FreeRTOS/Source/portable/GCC/RaspberryPi/
 CFLAGS += -I $(BASE)FreeRTOS/Source/include/
@@ -17,4 +18,5 @@ CFLAGS += -I $(BASE)Drivers/
 CFLAGS += -I $(BASE)Drivers/lan9514/include/
 CFLAGS += -I $(BASE)Drivers/FreeRTOS-Plus-TCP/include/
 
-TOOLCHAIN=armv7a-hardfloat-linux-gnueabi-
+#TOOLCHAIN=armv7a-hardfloat-linux-gnueabi-
+TOOLCHAIN=arm-softfloat-linux-gnueabi-
